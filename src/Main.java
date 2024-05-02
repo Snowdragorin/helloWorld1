6e0f4b762f409533af8d111b6a085de9c66fe3be
Elorin <bm.snizek@gmail.com>
2024-05-02 20:56:14 +0200

    Finish first lesson

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,28 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("sout + tab/enter");
+//Quick S.out.printl sout
+        printNumbers1to5();
+        double a = 3.5;
+        double b = 4.5;
+        System.out.println(calculateDiagonal(a,b));
+        double c;
+        c = calculateDiagonal(5,6);
+        System.out.println("Úhlopříčka trojúhelníku je: "+c);
+    }
 
+    private static void printNumbers1to5() {
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+    }
+
+    private static double calculateDiagonal(double a, double b) {
+        return Math.sqrt(a * a + b * b);
     }
 }
